6e31ffed0e670912e720c694ec974dff874f80ac
DanielQuarrell <Daniel2.Quarrell@live.uwe.ac.uk>
2020-01-24 15:34:19 +0000

    Added a terrain to the project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player instance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+    }
+}
---
+++ Assets/Scripts/WorldChunk.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldChunk : MonoBehaviour
+{
+    [SerializeField] GameObject model;
+
+    // Update is called once per frame
+    void Update()
+    {
+        model.SetActive(Vector3.Distance(this.transform.position, Player.instance.transform.position) < 10);
+    }
+}
